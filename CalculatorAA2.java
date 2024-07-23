95fa0d65dabc6b7a988c4b48bac47799b8d75fbe
Adnan Siddiqui <sid.adnan9569@gmail.com>
2024-07-23 22:53:19 +0530

    Add files via upload

---
+++ CalculatorAA2.java
@@ -0,0 +1,11 @@
+public class CalculatorAA2 
+{
+	public static void main(String[] args)
+	{
+		CalculatorExtends cal=new CalculatorExtends();
+		cal.add(12,15);
+		cal.multiply(17,3);
+		cal.substract(50,33);
+		cal.divide(70, 10);
+	}
+}
---
+++ CalculatorExtends.java
@@ -0,0 +1,11 @@
+public class CalculatorExtends extends Calculator
+{
+	public void substract(int x,int y)
+	{
+		System.out.println(x-y);
+	}
+	public void divide(int x,int y)
+	{
+		System.out.println(x/y);
+	}
+}
